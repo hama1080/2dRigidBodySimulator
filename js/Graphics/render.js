fe2c88c7c31ee8f3e0d082efb07b013312a6a35c
hmhm903 <hmhm903m@gmail.com>
2017-06-13 11:39:16 +0900

    Implement RenderCircle and RenderBox function. They don't support rotation yet.

--- js/Graphics/render.js
+++ js/Graphics/render.js
@@ -1,15 +1,32 @@
 function RenderCircle(context, center_pos, radius, rotation)
 {
+  //not support rotational circle
+  context.beginPath();
+  context.arc(center_pos.x, center_pos.y, radius, 0, Math.PI*2, false);
+  context.stroke();
+  //todo draw rotational circle as render line from center to edge
 }
 
 function RenderBox(context, center_pos, half_extents, rotation)
 {
+  //not support rotational box
+  pt0 = new Vector(center_pos.x - half_extents.x, center_pos.y - half_extents.y);
+  pt1 = new Vector(center_pos.x - half_extents.x, center_pos.y + half_extents.y);
+  pt2 = new Vector(center_pos.x + half_extents.x, center_pos.y + half_extents.y);
+  pt3 = new Vector(center_pos.x + half_extents.x, center_pos.y - half_extents.y);
+
+  context.beginPath();
+  context.moveTo(pt0.x, pt0.y);
+  context.lineTo(pt1.x, pt1.y);
+  context.lineTo(pt2.x, pt2.y);
+  context.lineTo(pt3.x, pt3.y);
+  context.closePath();
+  context.stroke();
 }
 
 onload = function(){
   draw();
 };
-
 
 function draw(){
   var canvas = document.getElementById('canvas');
@@ -18,11 +35,8 @@ function draw(){
   }
 
   var ctx = canvas.getContext('2d');
-  ctx.beginPath();
-  ctx.moveTo(20, 20);
-  ctx.lineTo(120, 20);
-  ctx.lineTo(120, 120);
-  ctx.lineTo(20, 120);
-  ctx.closePath();
-  ctx.stroke();
+  center = new Vector(50, 50);
+  half_ex = new Vector(20, 20);
+  RenderBox(ctx, center, half_ex, 0 );
+  RenderCircle(ctx, center, 50, 0);
 }
