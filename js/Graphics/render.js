4b7e655d3150bc54d9c6247ad3741a6d41d206ed
hmhm903 <hmhm903m@gmail.com>
2017-06-15 15:37:13 +0900

    Rotation test on index.html

--- js/Graphics/render.js
+++ js/Graphics/render.js
@@ -1,4 +1,4 @@
-function RenderCircle(context, center_pos, radius, rotation)
+function RenderCircle(context, center_pos, radius, angle)
 {
   //not support rotational circle
   context.beginPath();
@@ -7,13 +7,19 @@ function RenderCircle(context, center_pos, radius, rotation)
   //todo draw rotational circle as render line from center to edge
 }
 
-function RenderBox(context, center_pos, half_extents, rotation)
+function RenderBox(context, center_pos, half_extents, angle)
 {
   //not support rotational box
   pt0 = new Vector(center_pos.x - half_extents.x, center_pos.y - half_extents.y);
   pt1 = new Vector(center_pos.x - half_extents.x, center_pos.y + half_extents.y);
   pt2 = new Vector(center_pos.x + half_extents.x, center_pos.y + half_extents.y);
   pt3 = new Vector(center_pos.x + half_extents.x, center_pos.y - half_extents.y);
+
+  var mat = CreateRotationMatrix(angle);
+  pt0 = mat.multiple_vec(pt0);
+  pt1 = mat.multiple_vec(pt1);
+  pt2 = mat.multiple_vec(pt2);
+  pt3 = mat.multiple_vec(pt3);
 
   context.beginPath();
   context.moveTo(pt0.x, pt0.y);
@@ -41,9 +47,9 @@ function RenderLoop()
   var tmpCnt = 0;
   function draw(){
     ctx.clearRect(0, 0, width, height);
-    center = new Vector(50, 50);
-    half_ex = new Vector(20 + tmpCnt, 20);
-    RenderBox(ctx, center, half_ex, 0 );
+    center = new Vector(150, 150);
+    half_ex = new Vector(20, 20);
+    RenderBox(ctx, center, half_ex, tmpCnt / 100 );
     RenderCircle(ctx, center, 50, 0);
     tmpCnt++;
   }
