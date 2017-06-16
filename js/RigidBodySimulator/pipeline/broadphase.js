715aa4b0236175f789157fdfd0cc3723a022a8f7
hmhm903 <hmhm903m@gmail.com>
2017-06-16 19:17:22 +0900

    Start writing broadphase process.

---
+++ js/RigidBodySimulator/pipeline/broadphase.js
@@ -0,0 +1,34 @@
+function BroadPhase(
+  state_array,
+  collidable_array,
+  num_rigid_bodie
+)
+{
+  //find AABB intersection pair
+  for(var i = 0; i != num_rigid_bodie; i++)
+  {
+    for(var j = 0; j != num_rigid_bodie; j++)
+    {
+      stateA = state_array[i];
+      collidableA = collidable_array[i];
+      stateB = state_array[j];
+      collidableB = collidable_array[i];
+
+      var rotA = stateA.rot;
+      var centerA = stateA.pos + rot.multiple_vec(collidableA.center);
+      //var halfA = //expand AABB
+
+      var rotB = stateB.rot;
+      var centerB = stateB.pos + rot.multiple_vec(collidableB.center);
+      //var halfB = //expand AABB
+
+      //check intersect A, B
+    }
+  }
+
+  //sort
+
+  //comparison pair
+
+  //sort
+}
