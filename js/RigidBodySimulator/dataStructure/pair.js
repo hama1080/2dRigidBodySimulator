47085a9fbd3aadcdb6ec471196cc599f4f474210
hmhm903 <hmhm903m@gmail.com>
2017-06-19 20:15:31 +0900

    Modify Pair class that can hold unique key.

--- js/RigidBodySimulator/dataStructure/pair.js
+++ js/RigidBodySimulator/dataStructure/pair.js
@@ -1,11 +1,13 @@
 // Pair consists of two rigid body indices and an information of contact
+var key_num = 0;
 class Pair
 {
-  constructor(key, index0, index1, contact_info)
+  constructor(index0, index1, contact_info)
   {
-    this._key = key;       //unique key
+    this._key = key_num;       //unique key
     this._index0 = index0;
     this._index1 = index1;
     this._contact_info = contact_info;
+    key_num++;
   }
 }
