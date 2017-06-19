206049ceac18d2f86774ed89eaaaacfefa5bbb8c
hmhm903 <hmhm903m@gmail.com>
2017-06-19 20:17:23 +0900

    Revise finding AABB intersection pair

--- js/RigidBodySimulator/pipeline/broadphase.js
+++ js/RigidBodySimulator/pipeline/broadphase.js
@@ -12,9 +12,10 @@ function BroadPhase(
 )
 {
   //find AABB intersection pair
+  var pair_array = []
   for(var i = 0; i != num_rigid_bodie; i++)
   {
-    for(var j = 0; j != num_rigid_bodie; j++)
+    for(var j = i+1; j != num_rigid_bodie; j++)
     {
       stateA = state_array[i];
       collidableA = collidable_array[i];
@@ -22,21 +23,26 @@ function BroadPhase(
       collidableB = collidable_array[i];
 
       var expand_half = new Vector(0.01, 0.01);
-      var rotA = stateA.rot;
-      var centerA = stateA.pos + rot.multiple_vec(collidableA.center);
+      var rotA = stateA.rotation;
+      var centerA = stateA.pos + rotA.multiple_vec(collidableA.AABB.center);
       var halfA = collidableA.AABB.half +  expand_half; //expand AABB
 
-      var rotB = stateB.rot;
-      var centerB = stateB.pos + rot.multiple_vec(collidableB.center);
+      var rotB = stateB.rotation;
+      var centerB = stateB.pos + rotB.multiple_vec(collidableB.AABB.center);
       var halfB = collidableB.AABB.half + expand_half;  //expand AABB
 
       //check intersect A, B
       if(CheckIntersectAABB(centerA, halfA, centerB, halfB))
       {
         //create pair
+        var index0 = i < j ? i : j;
+        var index1 = i < j ? j : i;
+        var pair = new Pair(i, j, null);
+        pair_array.push(pair);
       }
     }
   }
+  console.log(pair);
 
   //sort
 
